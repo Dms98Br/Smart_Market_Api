f5e479aa0c81e37b5d9f6c5bbe5dc7bb41704f13
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2021-02-09 23:45:30 -0300

    Update app.js
    Createed route for product

--- src/app.js
+++ src/app.js
@@ -2,30 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./configure/keyaccess');
 const bodyParser = require('body-parser');
+const customerRoute = require('./route/customer-route');
+const listRoute = require('./route/list-route');
+const productRoute = require('./route/product-route');
 
 const app = express();
 const router = express.Router();
 
 //Conecta ao banco
 mongoose.connect(config.connectionString,{useNewUrlParser: true, useUnifiedTopology: true});
-var db = mongoose.connection;
+// var db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "connection error:"));
+// db.on("error", console.error.bind(console, "connection error:"));
 
-db.once("open", function() {
-  console.log("Connection Successful!");
-});
+// db.once("open", function() {
+//   console.log("Connection Successful!");
+// });
 
-// Carrega os models
-const Customer = require('./models/customer');
-
-//Carregar as rotas
-const customerRoute = require('./route/customer-route');
-const listRoute = require('./route/list-route');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 app.use('/customers', customerRoute);
 app.use('/lists', listRoute);
+app.use('/product', productRoute);
 
 module.exports = app;

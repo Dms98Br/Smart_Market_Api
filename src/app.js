592636320f6c1e2a56b5c578572ac0311542a7f6
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2021-02-09 21:37:16 -0300

    Update app.js
    Create route for lists

--- src/app.js
+++ src/app.js
@@ -15,26 +15,17 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   console.log("Connection Successful!");
 });
-// var schema = mongoose.Schema({
-//   name: String,
-//   age: Number
-// });
-// var Model = mongoose.model("model", schema, "myCollection");
 
-// var doc1 = new Model({ name: "John", age: 21 });
-
-// doc1.save(function(err, doc) {
-//   if (err) return console.error(err);
-//   console.log("Document inserted succussfully!");
-// });
 // Carrega os models
 const Customer = require('./models/customer');
 
 //Carregar as rotas
 const customerRoute = require('./route/customer-route');
+const listRoute = require('./route/list-route');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 app.use('/customers', customerRoute);
+app.use('/lists', listRoute);
 
 module.exports = app;

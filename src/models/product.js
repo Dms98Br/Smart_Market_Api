544d2f55aa08ede0e5ef18f877314ffa59685042
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2021-02-09 23:42:16 -0300

    Create product.js

---
+++ src/models/product.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  nameProduct: {
+    type: String,
+    required: ['true', 'O nome da lista é obrigatório'],
+    trim: true
+  },
+  brandProduct: {
+    type: String,
+    trim: true
+  },
+  quantity: {
+    type: String,
+    trim: true
+  },
+  typeUnity: [{
+    type: String,
+    required: ['true', 'Unidade de medida é obrigatório'],
+    trim: true
+  }],
+  id_list: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: ['true', 'Id da lita é obrigatório'],
+  },
+  status: [{
+    type: String,
+    required: true,
+    enum: ['true', 'false'],
+    default: 'true'
+  }],
+});
+
+module.exports = mongoose.model('products', schema);

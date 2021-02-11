28d2b1547876e961de39f26b282b62592c1b4408
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2021-02-11 00:15:06 -0300

    Remove customer-repository
    customer-controller has been adapted to use without the customer-repository

--- src/controllers/customer-controllers.js
+++ src/controllers/customer-controllers.js
@@ -1,37 +1,35 @@
-const User = require("../models/customer");
-const bcrypt = require('bcryptjs');
-const { response } = require("express");
-const repository = require('../repositories/customer-repository');
-exports.post = async(req, res, next)=>{
-  //console.log(req.body)
+const Customer = require("../models/customer");
+
+exports.create = async (req, res, next) => {
+  const customer = new Customer(req.body);
+  const { email, password } = req.body;
   try {
-    const { email } = req.body;
-    if (await User.findOne({ email }))
-        return res.status(400).send({ message: 'E-mail já cadastrado '})
+    if (await Customer.findOne({ email }))
+      return res.status(400).send({ message: 'E-mail já cadastrado ' })
     else
-      await repository.create(req.body);
+      customer.password = customer.generateHash(password)
+    await customer.save();
       res.status(201).send({ menssage: 'Usuário criado com sucesso'});
       
   } catch (e) {
-    res.status(400).send({menssage: 'Erro ao cadastrar', e});
+    console.log(e);
+    res.status(400).send({ menssage: 'Erro ao cadastrar' });
   }
 }
 exports.get = async(req, res)=>{
   try {
-    var data = await repository.get()
-    res.status(200).send({
-      data: data
-    })
+    const customer = await Customer.find({});
+    res.status(200).send(customer)
   } catch (e) {
     res.status(400).send({menssage: 'Erro na solicitação', e});
   }
 }
 exports.getById = async(req, res)=>{
   try {
-       var data = await repository.getById(req.params.id);
-       res.status(200).send(data);
+    return res.send(await Customer.findById(req.params.id))
    } catch (e) {
-       res.status(500).send({
+    console.log(e);   
+    res.status(500).send({
            message: 'Falha ao processar sua requisição',
            error: e
        })
@@ -40,21 +38,29 @@ exports.getById = async(req, res)=>{
 
 exports.update = async(req, res) =>{
   try {
-    await repository.update(req.params.id, req.body);
+    await Customer.updateOne({ _id: req.params.id }, req.body)
     res.status(200).send('Atualização realizada com sucesso!')
   } catch (e) {
+    res.status(400).send({ menssage: 'Erro na solicitação', e });
+  }
+}
+
+exports.updatePassword = async (req, res) => {
+  try {
+    const customer = new Customer(req.body);
+    const { password } = req.body;
+    req.body.password = customer.generateHash(password)
+    await Customer.updateOne({ _id: req.params.id }, req.body)
+    res.status(200).send('Atualização realizada com sucesso!')
+  } catch (e) {
+    console.log(e);
     res.status(400).send({menssage: 'Erro na solicitação', e});
   }
 }
 
 exports.deleteForever = async(req, res)=>{
-  try {
-    if(! await User.findOne({_id: req.params.id}))
-      {
-        res.status(400).send({ menssage: 'Usuário não encontrado'});
-        return false;
-      }
-    var data = await repository.deleteForever(req.params.id);
+  try {    
+    await Customer.deleteOne({ _id: req.params.id })
     res.status(200).send({menssage: 'Usuário deletado com sucesso !'})
   } catch (e) {
     console.log(e);

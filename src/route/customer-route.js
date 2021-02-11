3f4f58b68e19ca73107f1f6812d7f9605af1a2b9
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2021-02-11 00:19:52 -0300

    Update customer-route.js
    Rota removida userController.post

--- src/route/customer-route.js
+++ src/route/customer-route.js
@@ -2,10 +2,11 @@ const express = require('express');
 var router = express.Router();
 const userController = require('../controllers/customer-controllers');
 
-router.post('/create', userController.post);
+router.post('/create', userController.create);
 router.get('/', userController.get);
 router.get('/:id', userController.getById);
 router.put('/:id', userController.update);
+router.put('/updatePassword/:id', userController.updatePassword);
 router.delete('/:id', userController.deleteForever);
 
 module.exports = router;

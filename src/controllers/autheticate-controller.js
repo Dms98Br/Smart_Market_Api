c10186849e9343041fa61bd54560ff14807b53dd
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2021-02-13 09:12:49 -0300

    createed authenticate with token

---
+++ src/controllers/autheticate-controller.js
@@ -0,0 +1,27 @@
+const Customer = require('../models/customer');
+const jwt = require('jsonwebtoken');
+const config = require('../configure/keyaccess')
+
+exports.authenticate = async (req, res) => {
+  try {
+    //const customer = new Customer(req.body);
+    const { email, password } = req.body;
+    const customer = await Customer.findOne({ email }).select('+password');
+
+    const id = customer._id
+    if (!customer)
+      res.status(400).send({ menssage: 'Usuário não foi encontrado' });
+
+    if (customer.email !== email)
+      res.status(400).send({ menssage: 'Email incorreto' });
+
+    if (!customer.validPassword(password))
+      res.status(400).send({ menssage: 'Senha incorreta' })
+
+    var token = ({ token: jwt.sign({ id }, config.SaltKey, { expiresIn: 86400 }), customer: customer })
+    res.status(202).send({ token: token })
+
+  } catch (err) {
+    res.status(400).json({ error: { code: err.code, message: err.message } });
+  }
+}
---
+++ src/services/authenticate-service.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const config = require('../configure/keyaccess')
+
+exports.authorize = (req, res, next) => {
+  var token = req.body.token || req.headers['x=access-token'];
+  if (!token)
+    return res.status(401).send({ auth: false, menssage: 'Acesso Restrito' });
+  else {
+    jwt.verify(token, config.SaltKey, (error, decode) => {
+      if (error)
+        res.status(401).json({ auth: false, message: 'Token inválido' });
+      else {
+        req.userId = decode.id;
+        next();
+      }
+    });
+  }
+
+}

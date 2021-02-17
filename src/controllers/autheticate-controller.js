c6be0faf2362b78528641ed1ebbbaa1946666dc6
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2021-02-17 20:49:01 -0300

    Update autheticate-controller.js
    Created verification for customer._id

--- src/controllers/autheticate-controller.js
+++ src/controllers/autheticate-controller.js
@@ -6,16 +6,16 @@ exports.authenticate = async (req, res) => {
   try {
     const { email, password } = req.body;
     const customer = await Customer.findOne({ email }).select('+password');    
-    const id = customer._id
-    if (!customer)
+    if (!customer || !customer._id)
       res.status(400).send({ menssage: 'Usuário não foi encontrado' });
 
-    if (customer.email !== email)
+      if (customer.email !== email)
       res.status(400).send({ menssage: 'Email incorreto' });
-
-    if (!customer.validPassword(password))
+      
+      if (!customer.validPassword(password))
       res.status(400).send({ menssage: 'Senha incorreta' })
-
+      
+    const id = customer._id
     var token = ({ token: jwt.sign({ id }, config.SaltKey, { expiresIn: 86400 }), customer: customer })
     res.status(202).send({ token: token })
 
